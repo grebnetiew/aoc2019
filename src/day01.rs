1ff48b34ae02f53e3fadae1f100e0f70dad4d157
Erik Weitenberg <eweitenberg@gmail.com>
2019-12-01 13:49:16 +0100

    Explanation of cargo-aoc and my method

--- src/day01.rs
+++ src/day01.rs
@@ -1,16 +1,26 @@
 use aoc_runner_derive::{aoc, aoc_generator};
 use std::num::ParseIntError;
 
+// An AOC Generator turns the input - read into a string - into something meaningful.
+// In this case, there is one number per line, so we want a vector of numbers (or,
+// if one of the lines contains garbage, an error).
 #[aoc_generator(day1)]
 fn number_per_line(input: &str) -> Result<Vec<u32>, ParseIntError> {
     input.lines().map(|l| l.trim().parse()).collect()
 }
 
+// A solver solves one of the two puzzles.
+// This one solves Part 1.
+// (Get a list of parts' mass; each part needs floow(m / 3 - 2) fuel, compute
+// how much fuel is needed.)
 #[aoc(day1, part1)]
 fn solver1(input: &[u32]) -> u32 {
     input.iter().map(|mass| mass / 3 - 2).sum()
 }
 
+// A naive solution for part 2, which uses checked overflow to abort
+// if a subtraction runs below 0.
+// (Same as Part 1, but you need fuel for the fuel (per part) as well.)
 #[aoc(day1, part2)]
 fn solver2(input: &[u32]) -> u32 {
     input
@@ -27,6 +37,10 @@ fn solver2(input: &[u32]) -> u32 {
         .sum()
 }
 
+// I didn't like this, so I tried to implement an iterator which gives the
+// successive additional fuel requirement. So first the fuel for the part,
+// then the fuel for the previous amount of fuel, and so on.
+// It stops once it reaches 0 fuel.
 struct Fueler(u32);
 impl Iterator for Fueler {
     type Item = u32;
@@ -37,11 +51,14 @@ impl Iterator for Fueler {
     }
 }
 
+// That way we can just turn each part's mass into a Fueler iterator, and sum
+// all the fuel values it spits out. Neat!
 #[aoc(day1, part2, Iterator)]
 fn solver2a(input: &[u32]) -> u32 {
     input.iter().map(|&mass| Fueler(mass).sum::<u32>()).sum()
 }
 
+// For the tests, I just used the examples given in the puzzle text.
 #[cfg(test)]
 mod tests {
     use super::*;
