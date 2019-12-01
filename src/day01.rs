a6205625b27d2b8ead77fe0cd9f7a1c3446ec5dc
Erik <eweitenberg@gmail.com>
2019-12-01 21:35:18 +0100

    added recursive variant

--- src/day01.rs
+++ src/day01.rs
@@ -11,7 +11,7 @@ fn number_per_line(input: &str) -> Result<Vec<u32>, ParseIntError> {
 
 // A solver solves one of the two puzzles.
 // This one solves Part 1.
-// (Get a list of parts' mass; each part needs floow(m / 3 - 2) fuel, compute
+// (Get a list of parts' mass; each part needs floor(m / 3 - 2) fuel, compute
 // how much fuel is needed.)
 #[aoc(day1, part1)]
 fn solver1(input: &[u32]) -> u32 {
@@ -21,26 +21,38 @@ fn solver1(input: &[u32]) -> u32 {
 // A naive solution for part 2, which uses checked overflow to abort
 // if a subtraction runs below 0.
 // (Same as Part 1, but you need fuel for the fuel (per part) as well.)
-#[aoc(day1, part2)]
+#[aoc(day1, part2, Loop)]
 fn solver2(input: &[u32]) -> u32 {
-    input
-        .iter()
-        .map(|&mass| {
-            let mut mass = mass;
-            let mut totalfuel = 0u32;
-            while let Some(new_mass) = (mass / 3).checked_sub(2) {
-                mass = new_mass;
-                totalfuel += mass;
-            }
-            totalfuel
-        })
-        .sum()
+    input.iter().map(|&mass| totalmass(mass)).sum()
 }
 
-// I didn't like this, so I tried to implement an iterator which gives the
-// successive additional fuel requirement. So first the fuel for the part,
-// then the fuel for the previous amount of fuel, and so on.
-// It stops once it reaches 0 fuel.
+fn totalmass(mut mass: u32) -> u32 {
+    let mut totalfuel = 0u32;
+    while let Some(new_mass) = (mass / 3).checked_sub(2) {
+        mass = new_mass;
+        totalfuel += mass;
+    }
+    totalfuel
+}
+
+// While not inelegant, I didn't think the loop-based solution was very idiomatic.
+// I decided to experiment with some more readable/neat/galaxybrained versions.
+#[aoc(day1, part2, Recursion)]
+fn solver2a(input: &[u32]) -> u32 {
+    input.iter().map(|&mass| recursivemass(mass)).sum()
+}
+
+fn recursivemass(part: u32) -> u32 {
+    match (part / 3).saturating_sub(2) {
+        0 => 0,
+        fuel => fuel + recursivemass(fuel),
+    }
+}
+
+// Recursion is sometimes considered inefficient, so I tried to implement an
+// iterator which gives the successive additional fuel requirements per part.
+// So first the fuel for the part, then the fuel for the previous amount of
+// fuel, and so on. It stops once it reaches 0 fuel.
 struct Fueler(u32);
 impl Iterator for Fueler {
     type Item = u32;
@@ -54,7 +66,7 @@ impl Iterator for Fueler {
 // That way we can just turn each part's mass into a Fueler iterator, and sum
 // all the fuel values it spits out. Neat!
 #[aoc(day1, part2, Iterator)]
-fn solver2a(input: &[u32]) -> u32 {
+fn solver2b(input: &[u32]) -> u32 {
     input.iter().map(|&mass| Fueler(mass).sum::<u32>()).sum()
 }
 
@@ -79,5 +91,8 @@ mod tests {
         assert_eq!(solver2a(&[14]), 2);
         assert_eq!(solver2a(&[1969]), 966);
         assert_eq!(solver2a(&[100756]), 50346);
+        assert_eq!(solver2b(&[14]), 2);
+        assert_eq!(solver2b(&[1969]), 966);
+        assert_eq!(solver2b(&[100756]), 50346);
     }
 }
