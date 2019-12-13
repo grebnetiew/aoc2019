aef909afdb627ff9a37b14e265dade85a5ffe136
Erik <eweitenberg@gmail.com>
2019-12-13 13:28:11 +0100

    Solved day 12 after a major refactor

--- src/day12.rs
+++ src/day12.rs
@@ -17,145 +17,122 @@ fn positions(input: &str) -> Result<Vec<Vec<i64>>, ParseIntError> {
         .collect()
 }
 
-#[aoc(day12, part1)]
-fn solver1(positions: &[Vec<i64>]) -> i64 {
-    let mut positions = positions.to_vec();
-    let mut velocities = vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]];
-    for _n in 0..1000 {
-        // calculate velocities
-        for i in 0..4 {
-            for j in 0..4 {
-                for k in 0..3 {
-                    if positions[i][k] != positions[j][k] {
-                        if positions[i][k] < positions[j][k] {
-                            velocities[i][k] += 1;
-                        } else {
-                            velocities[i][k] -= 1;
-                        }
-                    }
-                }
-            }
-        }
-        // Update positions
-        for i in 0..4 {
-            for k in 0..3 {
-                positions[i][k] += velocities[i][k];
-            }
+fn one_coord_one_round(positions: &mut Vec<i64>, velocities: &mut Vec<i64>) {
+    for i in 0..4 {
+        for j in 0..4 {
+            velocities[i] += (positions[j] - positions[i]).signum();
         }
     }
+
+    for i in 0..4 {
+        positions[i] += velocities[i];
+    }
+}
+
+fn coordinate_loop(starting_pos: &[i64], n_rounds: usize) -> (Vec<i64>, Vec<i64>) {
+    let mut positions = starting_pos.to_vec();
+    let mut velocities = vec![0, 0, 0, 0];
+
+    for _round in 0..n_rounds {
+        one_coord_one_round(&mut positions, &mut velocities);
+    }
+    (positions, velocities)
+}
+
+fn transpose(m: &[Vec<i64>]) -> Vec<Vec<i64>> {
+    let len_outer = m.len();
+    let len_inner = m[0].len();
+    (0..len_inner)
+        .map(|i| (0..len_outer).map(|j| m[j][i]).collect())
+        .collect()
+}
+
+fn energy_after_rounds(positions: &[Vec<i64>], rounds: usize) -> i64 {
+    let starting_positions_per_coord = transpose(positions);
+
+    // Simulate some rounds
+    let (pos_x, vel_x) = coordinate_loop(&starting_positions_per_coord[0], rounds);
+    let (pos_y, vel_y) = coordinate_loop(&starting_positions_per_coord[1], rounds);
+    let (pos_z, vel_z) = coordinate_loop(&starting_positions_per_coord[2], rounds);
+
     // Calculate energy
     (0..4)
-        .map(|i| -> i64 {
-            positions[i].iter().cloned().map(i64::abs).sum::<i64>()
-                * velocities[i].iter().cloned().map(i64::abs).sum::<i64>()
+        .map(|i| {
+            (pos_x[i].abs() + pos_y[i].abs() + pos_z[i].abs())
+                * (vel_x[i].abs() + vel_y[i].abs() + vel_z[i].abs())
         })
         .sum()
 }
 
-#[aoc(day12, part2)]
-fn solver2(positions: &[Vec<i64>]) -> i64 {
-    let mut positions = positions.to_vec();
-    let mut velocities = vec![vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0], vec![0, 0, 0]];
+#[aoc(day12, part1)]
+fn solver1(positions: &[Vec<i64>]) -> i64 {
+    energy_after_rounds(positions, 1000)
+}
 
-    let mut px_hist = Vec::new();
-    let mut py_hist = Vec::new();
-    let mut pz_hist = Vec::new();
-    let mut vx_hist = Vec::new();
-    let mut vy_hist = Vec::new();
-    let mut vz_hist = Vec::new();
+fn coordinate_loop_length(starting_pos: &[i64]) -> usize {
+    let starting_vel = [0, 0, 0, 0];
+    let mut positions = starting_pos.to_vec();
+    let mut velocities = starting_vel.to_vec();
 
-    let (mut loop_px, mut loop_py, mut loop_pz) = (0i64, 0i64, 0i64);
-    let (mut loop_vx, mut loop_vy, mut loop_vz) = (0i64, 0i64, 0i64);
-    let mut n = 0;
-    while loop_px == 0
-        || loop_py == 0
-        || loop_pz == 0
-        || loop_vx == 0
-        || loop_vy == 0
-        || loop_vz == 0
-    {
-        // calculate velocities
-        for i in 0..4 {
-            for j in 0..4 {
-                for k in 0..3 {
-                    if positions[i][k] != positions[j][k] {
-                        if positions[i][k] < positions[j][k] {
-                            velocities[i][k] += 1;
-                        } else {
-                            velocities[i][k] -= 1;
-                        }
-                    }
-                }
-            }
-        }
-        // Update positions
-        for i in 0..4 {
-            for k in 0..3 {
-                positions[i][k] += velocities[i][k];
-            }
-        }
-        let pos_x = (0..4).map(|i| positions[i][0]).collect::<Vec<_>>();
-        let pos_y = (0..4).map(|i| positions[i][1]).collect::<Vec<_>>();
-        let pos_z = (0..4).map(|i| positions[i][2]).collect::<Vec<_>>();
+    let mut rounds = 0;
+    loop {
+        one_coord_one_round(&mut positions, &mut velocities);
+        rounds += 1;
 
-        let vel_x = (0..4).map(|i| velocities[i][0]).collect::<Vec<_>>();
-        let vel_y = (0..4).map(|i| velocities[i][1]).collect::<Vec<_>>();
-        let vel_z = (0..4).map(|i| velocities[i][2]).collect::<Vec<_>>();
-
-        if loop_px == 0 {
-            if let Some(i) = px_hist.iter().position(|v| v == &pos_x) {
-                loop_px = n;
-                println!("Loop px is ({}, {}) {}", i, n, n - i as i64);
-            }
+        if positions == starting_pos && velocities == starting_vel {
+            return rounds;
         }
-        if loop_py == 0 {
-            if let Some(i) = py_hist.iter().position(|v| v == &pos_y) {
-                loop_py = n;
-                println!("Loop py is ({}, {}) {}", i, n, n - i as i64);
-            }
-        }
-        if loop_pz == 0 {
-            if let Some(i) = pz_hist.iter().position(|v| v == &pos_z) {
-                loop_pz = n;
-                println!("Loop pz is ({}, {}) {}", i, n, n - i as i64);
-            }
-        }
-        if loop_vx == 0 {
-            if let Some(i) = vx_hist.iter().position(|v| v == &vel_x) {
-                loop_vx = n;
-                println!("Loop vx is ({}, {}) {}", i, n, n - i as i64);
-            }
-        }
-        if loop_vy == 0 {
-            if let Some(i) = vy_hist.iter().position(|v| v == &vel_y) {
-                loop_vy = n;
-                println!("Loop vy is ({}, {}) {}", i, n, n - i as i64);
-            }
-        }
-        if loop_vz == 0 {
-            if let Some(i) = vz_hist.iter().position(|v| v == &vel_z) {
-                loop_vz = n;
-                println!("Loop vz is ({}, {}) {}", i, n, n - i as i64);
-            }
-        }
-
-        px_hist.push(pos_x);
-        py_hist.push(pos_y);
-        pz_hist.push(pos_z);
-        vx_hist.push(vel_x);
-        vy_hist.push(vel_y);
-        vz_hist.push(vel_z);
-        n += 1;
     }
-    loop_px
-        .lcm(&loop_py)
-        .lcm(&loop_pz)
-        .lcm(&loop_vx)
-        .lcm(&loop_vy)
-        .lcm(&loop_vz)
+}
+
+#[aoc(day12, part2)]
+fn solver2(positions: &[Vec<i64>]) -> usize {
+    let loop_lengths: Vec<_> = (0..3)
+        .map(|i| {
+            coordinate_loop_length(&positions.iter().map(|point| point[i]).collect::<Vec<_>>())
+        })
+        .collect();
+    loop_lengths[0].lcm(&loop_lengths[1]).lcm(&loop_lengths[2])
 }
 
 #[cfg(test)]
 mod tests {
-    // No tests in the puzzle text :(
+    use super::*;
+
+    #[test]
+    fn test_energy_after_rounds() {
+        assert_eq!(
+            energy_after_rounds(
+                &positions(
+                    "<x=-1, y=0, z=2>\n<x=2, y=-10, z=-7>\n<x=4, y=-8, z=8>\n<x=3, y=5, z=-1>"
+                )
+                .unwrap(),
+                10
+            ),
+            179
+        );
+        assert_eq!(
+            energy_after_rounds(
+                &positions(
+                    "<x=-8, y=-10, z=0>\n<x=5, y=5, z=10>\n<x=2, y=-7, z=3>\n<x=9, y=-8, z=-3>"
+                )
+                .unwrap(),
+                100
+            ),
+            1940
+        );
+    }
+
+    #[test]
+    fn test_loop_lengths() {
+        assert_eq!(
+            solver2(
+                &positions(
+                    "<x=-8, y=-10, z=0>\n<x=5, y=5, z=10>\n<x=2, y=-7, z=3>\n<x=9, y=-8, z=-3>"
+                )
+                .unwrap()
+            ),
+            4_686_774_924
+        );
+    }
 }
