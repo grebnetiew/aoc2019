f764901ef0a5f86c1c40b3c9d1b5b10eaa744cd2
Erik <eweitenberg@gmail.com>
2019-12-14 16:32:19 +0100

    Found a better way to write multiline strings

--- src/day14.rs
+++ src/day14.rs
@@ -153,19 +153,40 @@ mod tests {
 
     #[test]
     fn test_run1() {
-        assert_eq!(solver1(&reactions("10 ORE => 10 A\n1 ORE => 1 B\n7 A, 1 B => 1 C\n7 A, 1 C => 1 D\n7 A, 1 D => 1 E\n7 A, 1 E => 1 FUEL")), 31);
-        assert_eq!(solver1(&reactions("9 ORE => 2 A\n8 ORE => 3 B\n7 ORE => 5 C\n3 A, 4 B => 1 AB\n5 B, 7 C => 1 BC\n4 C, 1 A => 1 CA\n2 AB, 3 BC, 4 CA => 1 FUEL")), 165);
         assert_eq!(
             solver1(&reactions(
-                "157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+                "10 ORE => 10 A\n\
+                 1 ORE => 1 B\n\
+                 7 A, 1 B => 1 C\n\
+                 7 A, 1 C => 1 D\n\
+                 7 A, 1 D => 1 E\n\
+                 7 A, 1 E => 1 FUEL"
+            )),
+            31
+        );
+        assert_eq!(
+            solver1(&reactions(
+                "9 ORE => 2 A\n\
+                 8 ORE => 3 B\n\
+                 7 ORE => 5 C\n\
+                 3 A, 4 B => 1 AB\n\
+                 5 B, 7 C => 1 BC\n\
+                 4 C, 1 A => 1 CA\n\
+                 2 AB, 3 BC, 4 CA => 1 FUEL"
+            )),
+            165
+        );
+        assert_eq!(
+            solver1(&reactions(
+                "157 ORE => 5 NZVS\n\
+                 165 ORE => 6 DCFZ\n\
+                 44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n\
+                 12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n\
+                 179 ORE => 7 PSHF\n\
+                 177 ORE => 5 HKGWZ\n\
+                 7 DCFZ, 7 PSHF => 2 XJWVT\n\
+                 165 ORE => 2 GPVTF\n\
+                 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
             )),
             13312
         );
@@ -175,54 +196,54 @@ mod tests {
     fn test_run2() {
         assert_eq!(
             solver2(&reactions(
-                "157 ORE => 5 NZVS
-165 ORE => 6 DCFZ
-44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
-12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
-179 ORE => 7 PSHF
-177 ORE => 5 HKGWZ
-7 DCFZ, 7 PSHF => 2 XJWVT
-165 ORE => 2 GPVTF
-3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
+                "157 ORE => 5 NZVS\n\
+                 165 ORE => 6 DCFZ\n\
+                 44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL\n\
+                 12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ\n\
+                 179 ORE => 7 PSHF\n\
+                 177 ORE => 5 HKGWZ\n\
+                 7 DCFZ, 7 PSHF => 2 XJWVT\n\
+                 165 ORE => 2 GPVTF\n\
+                 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT"
             )),
             82_892_753
         );
         assert_eq!(
             solver2(&reactions(
-                "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
-17 NVRVD, 3 JNWZP => 8 VPVL
-53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
-22 VJHF, 37 MNCFX => 5 FWMGM
-139 ORE => 4 NVRVD
-144 ORE => 7 JNWZP
-5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
-5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
-145 ORE => 6 MNCFX
-1 NVRVD => 8 CXFTF
-1 VJHF, 6 MNCFX => 4 RFSQX
-176 ORE => 6 VJHF"
+                "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG\n\
+                 17 NVRVD, 3 JNWZP => 8 VPVL\n\
+                 53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL\n\
+                 22 VJHF, 37 MNCFX => 5 FWMGM\n\
+                 139 ORE => 4 NVRVD\n\
+                 144 ORE => 7 JNWZP\n\
+                 5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC\n\
+                 5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV\n\
+                 145 ORE => 6 MNCFX\n\
+                 1 NVRVD => 8 CXFTF\n\
+                 1 VJHF, 6 MNCFX => 4 RFSQX\n\
+                 176 ORE => 6 VJHF"
             )),
             5_586_022
         );
         assert_eq!(
             solver2(&reactions(
-                "171 ORE => 8 CNZTR
-7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
-114 ORE => 4 BHXH
-14 VRPVC => 6 BMBT
-6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
-6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
-15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
-13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
-5 BMBT => 4 WPTQ
-189 ORE => 9 KTJDG
-1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
-12 VRPVC, 27 CNZTR => 2 XDBXC
-15 KTJDG, 12 BHXH => 5 XCVML
-3 BHXH, 2 VRPVC => 7 MZWV
-121 ORE => 7 VRPVC
-7 XCVML => 6 RJRHP
-5 BHXH, 4 VRPVC => 5 LTCX"
+                "171 ORE => 8 CNZTR\n\
+                 7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL\n\
+                 114 ORE => 4 BHXH\n\
+                 14 VRPVC => 6 BMBT\n\
+                 6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL\n\
+                 6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT\n\
+                 15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW\n\
+                 13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW\n\
+                 5 BMBT => 4 WPTQ\n\
+                 189 ORE => 9 KTJDG\n\
+                 1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP\n\
+                 12 VRPVC, 27 CNZTR => 2 XDBXC\n\
+                 15 KTJDG, 12 BHXH => 5 XCVML\n\
+                 3 BHXH, 2 VRPVC => 7 MZWV\n\
+                 121 ORE => 7 VRPVC\n\
+                 7 XCVML => 6 RJRHP\n\
+                 5 BHXH, 4 VRPVC => 5 LTCX"
             )),
             460_664
         );
